f756e37830c8a4f466a87e39b0d936ee4eadcd2a
RiczCalixto <rcalixtos@gmail.com>
2019-11-20 12:58:50 -0300

    constructor na child, uso do super para chamar o constructor da classe pai

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -18,12 +18,17 @@ class Vehiicle {
 }
 
 class Car extends Vehiicle {
+  constructor(public wheels: number, color: string) {
+    super(color)
+
+  }
+
   startDrivingProcess(): void {
     this.honk();
   }
 }
 
-const caar = new Car('black');
+const caar = new Car(2, 'black');
 caar.drive()
 caar.startDrivingProcess()
 console.log(caar.color)
@@ -33,4 +38,3 @@ console.log(caar.color)
 // Private - This method can only be called by other methods in this class
 // Protected - This method can be called by other methods in this class, or by
 // other methods in child classes
-
