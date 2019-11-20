71b80342d8504b657eab208f6f16d2e96f52fe4f
Ric <rcalixtos@gmail.com>
2019-11-19 22:04:31 -0300

    add changes

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -26,3 +26,51 @@ let point: { x: number; y: number } = {
 const lgNumber: (nmber: number) => void = (nmber: number) => {
   console.log(nmber);
 };
+
+// When to use annotations
+// 1 - Function that returns 'any' type
+const json = '{"x":10, "y": 20}';
+const coordinates: {x:number, y:number} = JSON.parse(json);
+console.log(coordinates)
+
+//2 - When we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for(let i = 0; i < words.length; i++){
+  if(words[i] === 'green'){
+    foundWord = true;
+  }
+}
+
+//3 - quando queremos usar type que não pode ser inferido
+
+// anotations in functions
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny'
+}
+
+const logWeather = (forecast: {date: Date, weather: string}): void =>{
+  console.log(forecast.date);
+  console.log(forecast.weather);
+}
+
+interface Casa{
+  date: Date,
+  weather: string
+}
+
+const lgInterface = ({date, weather}: Casa) => {
+  console.log(date)
+  console.log(weather)
+};
+
+const lgDestruct = ({date, weather}: {date: Date, weather: string}) => {
+  console.log(date)
+  console.log(weather)
+};
+
+
+logWeather(forecast);
